098d3863b4146b4b4862a838fd3d0d0fe9c08db4
Muhammad Bangash <s092512@student.dtu.dk>
2020-10-25 18:26:22 +0100

    Konto klasse tilføjet.

---
+++ src/Konto.java
@@ -0,0 +1,21 @@
+public class Konto {
+    private int pengeBeholdning;
+
+    public Konto(int startPengeBeholdning){
+        this.pengeBeholdning = startPengeBeholdning;
+    }
+
+    public int getPengeBeholdning() {
+        return pengeBeholdning;
+    }
+
+    public boolean updatePengeBeholdning(int feltVærdi) {
+        int nyPengeBeholdning = pengeBeholdning + feltVærdi;
+        if(nyPengeBeholdning < 0){
+            return false;
+        }
+        pengeBeholdning = nyPengeBeholdning;
+        return true;
+    }
+}
+}
