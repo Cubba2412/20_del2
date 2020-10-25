399fa1fd6dd52fb362a670effb2639d8bc0d33ea
Muhammad Bangash <s092512@student.dtu.dk>
2020-10-25 18:28:06 +0100

    Spiller klasse tilføjet

---
+++ src/Spiller.java
@@ -0,0 +1,17 @@
+public class Spiller {
+
+    private String navn;
+    private Konto konto = new Konto(1000);
+
+    public Spiller(String navn){
+        this.navn = navn;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public Konto getKonto() {
+        return konto;
+    }
+}
